c0e21a4b1d00f1e90ecb8770fcffafc46b5e550c
Ram Krishna Yadav <itsramky234@gmail.com>
2025-09-19 00:53:54 +0545

    added new folder and toggle icons

--- frontend/src/components/CodeEditor/renderFilesTree.tsx
+++ frontend/src/components/CodeEditor/renderFilesTree.tsx
@@ -1,9 +1,13 @@
+import React,{useState} from "react";
 import { FileNode } from "@/types"
 import getExtension from "@/utils/getExtension";
 import getIconByExtension from "@/utils/getIconsByExtension";
 const renderFilesTree = (node: FileNode, setSelectedFile: React.Dispatch<React.SetStateAction<FileNode | null>>) => {
-
-
+    const [selectedFolder,setSelectedFolder] = useState<string | null>(null);
+    const [showFolderMenu,setShowFolderMenu]=useState<boolean>(false)
+    const handleCreateNewFile=()=>{
+        alert("New File Triggered")
+    }
     if (node.type === 'file') {
         const extension = getExtension(node.name);
         const icon = getIconByExtension(extension);
@@ -23,9 +27,28 @@ const renderFilesTree = (node: FileNode, setSelectedFile: React.Dispatch<React.S
     } else {
         return (
             <div key={node.id} className="folder-item">
-                <div className="drop-down">
-                    <span className="mr-2 text-xs">📁</span>
+                <div className="folder">
+                    <span
+                    onClick={()=>{
+                        if(selectedFolder === node.id){
+                            setSelectedFolder(null);
+                        }else{
+                            setSelectedFolder(node.id);
+                        }
+                    }}
+                    className="mr-2 text-md">{selectedFolder === node.id ? '▼' : '►'} 📁</span>
                     <span>{node.name}</span>
+                    <span className="text-md ml-2 cursor-pointer"
+                    onClick={handleCreateNewFile}
+                    >{/* Adding New File Icon here */}+</span>
+                    {showFolderMenu && (
+                        <div className="context-menu flex flex-col space-x-2 bg-[#161b22] p-2 rounded-md border border-[#30363d]/50 absolute">
+                            <button onClick={() => {/* Handle new file */}}>New File</button>
+                            <button onClick={() => {/* Handle new folder */}}>New Folder</button>
+                            <button onClick={() => {/* Handle rename */}}>Rename</button>
+                            <button onClick={() => {/* Handle delete */}}>Delete</button> 
+                        </div>
+                    )}
                 </div>
                 <div className="ml-4 border-l border-[#30363d]/20 pl-2">
                     {node.children && node.children.map(child => renderFilesTree(child, setSelectedFile))}
